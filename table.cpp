ef545f2bd4f336fd4363514a22a546e316c997fc
Xi Gong <xi@ucsb.edu>
2019-02-07 20:04:40 -0800

    error

--- table.cpp
+++ table.cpp
@@ -8,7 +8,63 @@
 
 using namespace std; 
 
-Table::Table(unsigned int max_entries = 100){
+string Table::toString(Entry e){
+	return std::to_string(e.get_key()) + " " + std::to_string(e.get_data()); 
+}
+
+void Table::mergeSort(Entry a[], size_t size){
+	size_t leftArraySize();
+	size_t rightArraySize();
+
+	if(size>1){
+		leftArraySize = size/2;
+		rightArraySize = size - leftArraySize; 
+		mergeSort((a+leftArraySize), rightArraySize);
+		
+		merge(a, leftArraySize, rightArraySize); 
+	}
+}
+
+void Table::merge(Entry a[], size_t leftArraySize, size_t rightArraySize){
+	int* tempArray;
+	size_t copied = 0;
+	size_t leftCopied = 0;
+	size_t rightCopied = 0;
+
+	tempArray = new Entry[leftArraySize + rightArraySize]; 
+	
+	while((leftCopied<leftArraySize) && (rightCopied < rightArraySize)){
+		if(a[leftCopied].get_key() < (a + rightArraySize)[rightCopied].get_key()){
+			tempArray[copied++] = a[leftCopied++]; 
+		}else{
+			tempArray[copied++] = (a + leftArraySize)[rightCopied++]; 
+		}
+	}
+	
+	while(leftCopied <leftArraySize){
+		tempArray[copied++] = a[leftCopied++]; 
+	}
+
+	while(rightCopied < rightArraySize){
+		tempArray[copied++] = (a + leftArraySize)[rightCopied++];
+	}
+
+	for(size_t i=0; i<leftArraySize + rightArraySize; i++){
+		a[i] = tempArray[i];
+	}
+
+	delete[] tempArray; 
+}
+
+int Table::hashFunction(int key) const{ //not sure why do I need to put const at the end; 
+	return key % 100;
+
+
+
+
+
+ 	
+Table::Table(unsigned int max_entries){ //do not redefine default parameter in cpp file. 
 	this->BUCKET = 100;
 	table = new list<Entry>[BUCKET];//used table as a pointer to the array of list;  
 }
@@ -61,75 +117,23 @@ bool Table::remove(unsigned int key){
 
 std::ostream& operator<<(std::ostream& out, const Table& t){
 	//place all Entry objects in the Hash table into a vector; 
-	int count; 
-
-	//second approach: array
-	Entry[] a = new Entry[count];
+	int size = t.BUCKET; 
+	int count = 0; 
+	Entry[] a = new Entry[size];
  	//repeat for loop
-	for(int i = 0; i < BUCKET; i++({
+	for(int i = 0; i < size; i++){
 		for(list<Entry>::iterator j = t[i].begin(); j != t[i].end(); j++){
 			a[count] = *j; 
 			count++; 
 		}
 	}
-	//MergeSort(e, count); 
+	mergeSort(a, count); 
 	for(int k = 0; k < count; k++){
 		out << a[k].toString(); 
 	}
 	return out; 
 }
 
-string Table::toString(Entry e){
-	return std::str(e.get_key()) + " " + std::str(e.get_data()); 
-}
-
-void Table::MergeSort(Entry a[], size_t size){
-	size_t leftArraySize();
-	size_t rightArraySize();
-
-	if(size>1){
-		leftArraySize = size/2;
-		rightArraySize = size - leftArraySize; 
-		mergesort((a+leftArraySize), rightArraySize);
-		
-		merge(a, leftArraySize, rightArraySize); 
-	}
-}
-
-void Table::merge(Entry a[], size_t leftArraySize, size_t rightArraySize){
-	int* tempArray;
-	size_t copied = 0;
-	size_t leftCopied = 0;
-	size_t rightCopied = 0;
-
-	tempArray = new int[leftArraySize + rightArraySize]; 
-	
-	while((leftCopied<leftArraySize) && (rightCopied < rightArraySize)){
-		if(a[leftCopied].get_key() < (a + rightArraySize)[rightCopied].get_key()){
-			tempArray[copied++] = a[leftCopied++]; 
-		}else{
-			tempArray[copied++] = (a + leftArraySize)[rightCopied++]; 
-		}
-	}
-	
-	while(leftCopied <leftArraySize){
-		tempArray[copied++] = a[leftCopied++]; 
-	}
-
-	while(rightCopied < rightArraySize){
-		tempArray[copied++] = (a + leftArraySize)[rightCopied++];
-	}
-
-	for(size_t i=0; i<leftArraySize + rightArraySize; i++){
-		a[i] = tempArray[i];
-	}
-
-	delete[] tempArray; 
-}
-
-int Table::hashFunction(int key){
-	return key % 100; 	
-}
 
 
 
--- table.h
+++ table.h
@@ -4,27 +4,32 @@
 //
 #ifndef table_h
 #define table_h
+#include <iostream>
 #include <list> 
 #include "entry.h"
+#include <string>
 
+
+using namespace std; 
 
 class Table{
 	public:
 	Table(unsigned int max_entries = 100);
 	
 	Table(unsigned int entries, std::istream& input);
-	
+	string toString(Entry e);
+		
 	void put(unsigned int key, std::string data);
 	void put(Entry e); 
 	std::string get(unsigned int key) const; 
 	bool remove(unsigned int key);
-        int hashFunction(int x); 
+        int hashFunction(int x) const; 
         void mergeSort(Entry a[], size_t size);
-	void merge(Entry a[], int a, int b);	
+	void merge(Entry a[], size_t l, size_t  r);	
 	
 	
 	friend std::ostream& operator<<(std::ostream& out, const Table& t); 
-	private:
+
 	list<Entry> *table; 
 	int BUCKET; 
 	
